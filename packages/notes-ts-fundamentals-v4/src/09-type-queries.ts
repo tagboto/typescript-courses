a62716079ac11a56898b1cf7d8c0e23d2dd47fe3
tagboto <zoe2.tagboto@gmail.com>
2025-04-05 17:08:34 +0100

    type queries

--- packages/notes-ts-fundamentals-v4/src/09-type-queries.ts
+++ packages/notes-ts-fundamentals-v4/src/09-type-queries.ts
@@ -1,44 +1,44 @@
 //* keyof
 
 type DatePropertyNames = keyof Date
-/*
-// type DateStringPropertyNames = DatePropertyNames & string
-// type DateSymbolPropertyNames = DatePropertyNames & symbol
+
+type DateStringPropertyNames = DatePropertyNames & string
+type DateSymbolPropertyNames = DatePropertyNames & symbol
 
 //* typeof
-/*
-// async function main() {
-//     const apiResponse = await Promise.all([
-//         fetch("https://example.com"),
-//         Promise.resolve("Titanium White"),
-//     ])
-//     type ApiResponseType = typeof apiResponse
-// }
-/*
-//?^ note: type alias within a function scope!
-// const MyAjaxConstructor = CSSRule
-// CSSRule.STYLE_RULE
-// const myAjax = new CSSRule()
 
+async function main() {
+    const apiResponse = await Promise.all([
+        fetch("https://example.com"),
+        Promise.resolve("Titanium White"),
+    ])
+    type ApiResponseType = typeof apiResponse
+}
+
+//?^ note: type alias within a function scope!
+const MyRule = CSSRule
+CSSRule.STYLE_RULE
+const foo = new MyRule()
+type MyRuleType = typeof MyRule
 
 //* Indexed Access Types
 
-/*
-// interface Car {
-//     make: string
-//     model: string
-//     year: number
-//     color: {
-//         red: string
-//         green: string
-//         blue: string
-//     }
-// }
 
-// let carColor: Car["color"] //✔️ Reaching for something that exists
-// let carSomething: Car["not-something-on-car"] //! Reaching for something invalid
-// let carColorRedComponent: Car["color"]["red"] //✔️ Reaching for something nested
-// let carProperty: Car["color" | "year"] // ✔️ Passing a union type through the index
+interface Car {
+    make: string
+    model: string
+    year: number
+    color: {
+        red: string
+        green: string
+        blue: string
+    }
+}
+
+let carColor: Car["color"] //✔️ Reaching for something that exists
+//let carSomething: Car["not-something-on-car"] //! Reaching for something invalid
+let carColorRedComponent: Car["color"]["red"] //✔️ Reaching for something nested
+let carProperty: Car["color" | "year"] // ✔️ Passing a union type through the index
 
 //* Use case: the type registry pattern
 /*
